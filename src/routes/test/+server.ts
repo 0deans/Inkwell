43a22426e721192e5d6c68162e879cb615792dfa
Denys <denusdednom5@gmail.com>
2024-05-05 15:17:59 +0300

    chore: Update database configuration and connection handling

--- src/routes/test/+server.ts
+++ src/routes/test/+server.ts
@@ -1,15 +1,9 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
 import type { RequestHandler } from '@sveltejs/kit';
 import { genres } from '$lib/server/schemas';
-import { DATABASE_URL } from '$env/static/private';
+import { db } from '$lib/server/db';
 
 export const GET: RequestHandler = async ({ platform }) => {
-	const sql = neon(DATABASE_URL);
-	const db = drizzle(sql);
 	const [onePost] = await db.select().from(genres);
-
 	const response = await platform?.env.AI.run('@cf/baai/bge-base-en-v1.5', { text: 'input' });
-
 	return new Response(JSON.stringify({ post: onePost, response }));
 };
