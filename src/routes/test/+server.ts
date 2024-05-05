8feaa5e8eb18bef253f7d04f9577ebf07543ce2f
Denys <denusdednom5@gmail.com>
2024-05-05 15:02:21 +0300

    test

--- src/routes/test/+server.ts
+++ src/routes/test/+server.ts
@@ -2,10 +2,14 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import type { RequestHandler } from '@sveltejs/kit';
 import { genres } from '$lib/server/schemas';
+import { DATABASE_URL } from '$env/static/private';
 
-export const GET: RequestHandler = async () => {
-	const sql = neon(process.env.DATABASE_URL!);
+export const GET: RequestHandler = async ({ platform }) => {
+	const sql = neon(DATABASE_URL);
 	const db = drizzle(sql);
 	const [onePost] = await db.select().from(genres);
-	return new Response(JSON.stringify({ post: onePost }));
+
+	const response = await platform?.env.AI.run('@cf/baai/bge-base-en-v1.5', { text: 'input' });
+
+	return new Response(JSON.stringify({ post: onePost, response }));
 };
